39f8fd165746a63bbef495f0b7d9dcdc6ccac0a1
Eyeruz <3yerus@gmail.com>
2020-10-05 21:31:42 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,31 @@
 // add code snippets from README
+let state
+
+let button = document.getElementById('button');
+ 
+function reducer(state  = {count: 0}, action){
+  switch (action.type) {
+    case '++':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+
+}
+function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+
+}
+
+function dispatch(action) {
+    state = reducer(state, action)
+    render()
+}
+
+button.addEventListener('click',() => {
+    dispatch({type: '++'})
+})
+
+
+dispatch({type: '++'})
